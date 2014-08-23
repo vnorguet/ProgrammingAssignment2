d9e49529b97b406e3a17ba0999960a4cba39be5e
Vincent NORGUET <vincent.norguet@gmail.com>
2014-08-23 12:23:44 +0200

    Add cacheable matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,43 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m = NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
+
+test_vnorguet_code <- function(){
+  testMatrix <- matrix(
+    c(2, 4, 3, 1, 5, 7, 2, 5, 1),
+    nrow=3,
+    ncol=3)
+  demo <- makeCacheMatrix(testMatrix)
+  ## Not cached here
+  cacheSolve(demo)
+  # Should be cached
+  cacheSolve(demo)
 }
